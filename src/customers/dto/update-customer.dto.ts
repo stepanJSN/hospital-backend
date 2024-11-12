cfcad06fcd7be71791df9c7c36eff9a39b1feddd
Stepan Stadniuk <stepanigcom@gmail.com>
2024-11-12 12:23:56 +0200

    made email unchangeable for customers

--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
+export class UpdateCustomerDto extends PartialType(
+  OmitType(CreateCustomerDto, ['email'] as const),
+) {}
