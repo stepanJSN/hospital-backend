e3564ae7fe164d25eac0bb81786288c8ef773ea1
Stepan Stadniuk <stepanigcom@gmail.com>
2024-11-14 12:32:40 +0200

    refactored staff module

--- src/staff/staff.service.ts
+++ src/staff/staff.service.ts
@@ -2,7 +2,6 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateStaffDto } from './dto/create-staff.dto';
 import { UpdateStaffDto } from './dto/update-staff.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import dayjs from 'dayjs';
 import { Role } from '@prisma/client';
 import { hash } from 'argon2';
 
@@ -12,20 +11,15 @@ type FindAllWhere = {
   };
   name?: {
     startsWith: string;
+    mode: 'insensitive';
   };
   surname?: {
     startsWith: string;
+    mode: 'insensitive';
   };
   specializationId?: string;
   specialization?: {
     isNot: null;
-  };
-  schedule?: {
-    some: {
-      dayOfWeek: {
-        equals: number;
-      };
-    };
   };
 };
 
@@ -55,66 +49,55 @@ export class StaffService {
     return id;
   }
 
-  // findAll(
-  //   role: Role,
-  //   specializationId?: string,
-  //   date?: string,
-  //   surname?: string,
-  //   name?: string,
-  // ) {
-  //   const where = () => {
-  //     const whereRes: FindAllWhere = {};
-  //     if (role !== Role.Admin) {
-  //       whereRes.role = {
-  //         not: Role.Admin,
-  //       };
-  //       whereRes.specialization = {
-  //         isNot: null,
-  //       };
-  //     }
-  //     if (specializationId) {
-  //       whereRes.specializationId = specializationId;
-  //     }
-  //     if (date) {
-  //       whereRes.schedule = {
-  //         some: {
-  //           dayOfWeek: {
-  //             equals: new Date(date).getDay(),
-  //           },
-  //         },
-  //       };
-  //     }
-  //     if (surname) {
-  //       whereRes.surname = {
-  //         startsWith: surname,
-  //       };
-  //     }
-  //     if (name) {
-  //       whereRes.name = {
-  //         startsWith: name,
-  //       };
-  //     }
-  //     return whereRes;
-  //   };
+  findAll(
+    role: Role,
+    specializationId?: string,
+    surname?: string,
+    name?: string,
+  ) {
+    const where: FindAllWhere = {};
+    if (role !== Role.Admin) {
+      where.role = {
+        not: Role.Admin,
+      };
+      where.specialization = {
+        isNot: null,
+      };
+    }
+    if (specializationId) {
+      where.specializationId = specializationId;
+    }
+    if (surname) {
+      where.surname = {
+        startsWith: surname,
+        mode: 'insensitive',
+      };
+    }
+    if (name) {
+      where.name = {
+        startsWith: name,
+        mode: 'insensitive',
+      };
+    }
 
-  //   const select = {
-  //     id: true,
-  //     name: true,
-  //     surname: true,
-  //     experience: true,
-  //     gender: true,
-  //     specialization: {
-  //       select: {
-  //         title: true,
-  //       },
-  //     },
-  //   };
+    const select = {
+      id: true,
+      name: true,
+      surname: true,
+      experience: true,
+      gender: true,
+      specialization: {
+        select: {
+          title: true,
+        },
+      },
+    };
 
-  //   return this.prisma.staff.findMany({
-  //     where: where(),
-  //     select: select,
-  //   });
-  // }
+    return this.prisma.staff.findMany({
+      where: where,
+      select: select,
+    });
+  }
 
   findAllAdmins() {
     return this.prisma.staff.findMany({
@@ -155,64 +138,84 @@ export class StaffService {
     return result;
   }
 
-  // async getAvailableTime(staffId: string, startDate: string, endDate: string) {
-  //   const startDateParsed = new Date(startDate);
-  //   const endDateParsed = new Date(endDate);
-  //   const dataDB = await this.prisma.staff.findUnique({
-  //     where: {
-  //       id: staffId,
-  //     },
-  //     select: {
-  //       schedule: true,
-  //       appointments: {
-  //         where: {
-  //           dateTime: {
-  //             gte: startDateParsed,
-  //             lte: endDateParsed,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   });
-  //   const result = [];
+  async getAvailableTime(staffId: string, startDate: string, endDate: string) {
+    const startDateParsed = new Date(startDate);
+    const endDateParsed = new Date(endDate);
 
-  //   for (
-  //     let index = new Date(startDateParsed);
-  //     index < endDateParsed;
-  //     index.setDate(index.getDate() + 1)
-  //   ) {
-  //     const day = dataDB.schedule.find(
-  //       (element) =>
-  //         element.dayOfWeek === index.getDay() && element.startTime !== null,
-  //     );
-  //     if (day) {
-  //       const oneDay = {
-  //         dayOfWeek: new Date(index),
-  //         startTime: day.startTime,
-  //         endTime: day.endTime,
-  //         bookedTime: [],
-  //       };
-  //       for (let i = day.startTime; i < day.endTime; i++) {
-  //         if (
-  //           dataDB.appointments.find((element) => {
-  //             const appointmentDate = dayjs(element.dateTime);
-  //             return (
-  //               appointmentDate.get('hours') === i &&
-  //               appointmentDate.get('day') === index.getDay()
-  //             );
-  //           })
-  //         ) {
-  //           oneDay.bookedTime.push(i);
-  //         }
-  //       }
-  //       result.push(oneDay);
-  //     } else {
-  //       result.push({ dayOfWeek: new Date(index) });
-  //     }
-  //   }
+    const { schedule, scheduleExceptions, appointments } =
+      await this.prisma.staff.findUnique({
+        where: {
+          id: staffId,
+        },
+        select: {
+          schedule: {
+            where: {
+              dayOfWeek: {
+                gte: startDateParsed.getDay(),
+                lte: endDateParsed.getDay(),
+              },
+            },
+          },
+          scheduleExceptions: true,
+          appointments: {
+            where: {
+              dateTime: {
+                gte: startDateParsed,
+                lte: endDateParsed,
+              },
+            },
+          },
+        },
+      });
 
-  //   return result;
-  // }
+    const availableTime: { date: string; availableSlots: number[] }[] = [];
+    const bookedHoursMap = new Map<string, Set<number>>();
+
+    appointments.forEach((appointment) => {
+      const dateKey = appointment.dateTime.toISOString().split('T')[0];
+      const hour = appointment.dateTime.getHours();
+
+      if (!bookedHoursMap.has(dateKey)) {
+        bookedHoursMap.set(dateKey, new Set());
+      }
+      bookedHoursMap.get(dateKey)?.add(hour);
+    });
+
+    for (
+      let d = new Date(startDateParsed);
+      d <= endDateParsed;
+      d.setDate(d.getDate() + 1)
+    ) {
+      const dayOfWeek = d.getDay();
+      const dateStr = d.toISOString().split('T')[0];
+
+      const exception = scheduleExceptions.find(
+        (ex) => ex.date.toISOString().split('T')[0] === dateStr,
+      );
+      if (exception) {
+        continue;
+      }
+
+      const daySchedule = schedule.find((sch) => sch.dayOfWeek === dayOfWeek);
+
+      if (daySchedule && daySchedule.from !== null && daySchedule.to !== null) {
+        const availableSlots: number[] = [];
+        const bookedHours = bookedHoursMap.get(dateStr) || new Set();
+
+        for (let hour = daySchedule.from; hour < daySchedule.to; hour++) {
+          if (!bookedHours.has(hour)) {
+            availableSlots.push(hour);
+          }
+        }
+
+        if (availableSlots.length > 0) {
+          availableTime.push({ date: dateStr, availableSlots });
+        }
+      }
+    }
+
+    return availableTime;
+  }
 
   async findOneByEmail(email: string) {
     return await this.prisma.user.findUnique({
